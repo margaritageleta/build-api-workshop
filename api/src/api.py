85214fa262753bc4c10e0e456292c9c834362f8c
3omni <noticiasmundiales99@gmail.com>
2019-04-24 19:35:47 +0200

    things

--- api/src/api.py
+++ api/src/api.py
@@ -1,6 +1,8 @@
 import datetime
+import string
+import requests
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file, render_template
 from flask_cors import CORS
 
 
@@ -23,3 +25,38 @@ def test():
         'text': 'This is a test.'
     }
     return jsonify(response), 200
+
+
+@flask_app.route('/name', methods=['GET'])
+def name():
+    response = {
+        'name': 'Rita'
+    }
+    return jsonify(response), 200
+
+@flask_app.route('/calculator', methods=['GET'])
+def calculator():
+    val1 = request.args.get('val1', type = int)
+    val2 = request.args.get('val2', type = int)
+    response = {
+        'result': val1 * val2
+    }
+    return jsonify(response), 200
+
+@flask_app.route('/downloader', methods=['GET'])
+def downloader():
+    url = request.args.get('url', type = str)
+    if url == None: ## No url, just /downloader
+        response = {
+            'error': True
+        }
+        return jsonify(response), 400
+    image = requests.get(url, stream = True)
+    with open ("src/imagen.jpeg", "wb") as file:
+        file.write(image.content)
+    response = {
+        'url': url
+    }
+    return jsonify(response), 200
+
+
